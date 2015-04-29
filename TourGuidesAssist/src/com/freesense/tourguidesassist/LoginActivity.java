9eb5e1ad855ca12e100095dd0d1e01771b38b6ab
litf <berserk.cn@gmail.com>
2015-04-29 10:21:32 +0800

    beta
    初次提交

---
+++ TourGuidesAssist/src/com/freesense/tourguidesassist/LoginActivity.java
@@ -0,0 +1,40 @@
+/**
+ * FreeSense.com.cn
+ * LoginActivity.java
+ * 
+ * 2015/04/28
+ * Create By LiTingfang
+ */
+package com.freesense.tourguidesassist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * @author LiTingfang
+ *
+ */
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_activity_layout);
+		
+		Button loginBtn = (Button)findViewById(R.id.signin_button);
+		loginBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(LoginActivity.this, "用户登录", Toast.LENGTH_SHORT).show();
+				
+				Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+}
---
+++ TourGuidesAssist/src/com/freesense/tourguidesassist/MainActivity.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.freesense.tourguidesassist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author LiTingfang
+ *
+ */
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity_layout);
+	}
+}
